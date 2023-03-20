8af13379639ae88a4c2f8113da8e911290e861ab
guials1714 <guials1714@naver.com>
2023-03-20 15:54:29 +0900

    해결

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from jinja2 import FileSystemLoader
 
 app = Flask(__name__)
+
+app.jinja_loader = FileSystemLoader('../frontend/public')
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 if __name__ == "__main__":
     app.run()
